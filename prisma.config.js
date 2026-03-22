ab7592cb7da6bf690c62f0772a3147eedb409045
Dubyyy <chidubem.chukwuafu@gmail.com>
2026-03-22 21:01:44 +0100

    admin page v-3

--- prisma.config.js
+++ prisma.config.js
@@ -1,6 +1,6 @@
 export default {
   schema: './prisma/schema.prisma',
   datasource: {
-    url: 'postgresql://neondb_owner:npg_l0dtAzS2EiKZ@ep-sparkling-pond-abw698us-pooler.eu-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require',
+    url: process.env.DATABASE_URL,
   },
 }
